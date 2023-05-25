6d891b92e61102f269f86146ff16747c82989885
“lcc” <chengcheng.li@51hgp.com>
2023-05-25 17:26:02 +0800

    feat: 添加tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,28 +7,19 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+<div class="p-3 flex w-full bg-white">
+      <div class="flex-1">
+        <h4 class="text-lg mb-2 text-gray-800">
+          商品管理
+        </h4>
+        <p class="text-xs text-gray-800">总计：30个分类。</p>
+      </div>
+      <div class="flex-1 text-right pt-3">
+        <button class="bg-indigo-500 hover:bg-indigo-700 text-xs text-white py-2 px-4 rounded shadow ml-1" @click="addGoods()">
+          添加商品
+        </button>
+      </div>
+    </div>
 </template>
 
 <style scoped>
