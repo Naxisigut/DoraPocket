b8e6d1160b0da30a9f51007ac10ad04a4c07ed1b
“lcc” <chengcheng.li@51hgp.com>
2023-05-26 23:14:46 +0800

    store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <GloabalSearch></GloabalSearch>
+  <div class="container bg-slate-400/75 p-2 mx-auto" >
+    <GloabalSearch></GloabalSearch>
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/components/GloabalSearch.vue
+++ src/components/GloabalSearch.vue
@@ -1,10 +1,21 @@
 <template>
-  <input type="text" class=" border-dashed ">
+  <div class="flex justify-center">
+    <!-- <input type="text" class=" h-8"> -->
+    <button class="w-1/2 max-w-xs h-5">
+      <SearchIcon></SearchIcon>
+    </button>
+  </div>
+
+  
 </template>
 
 <script setup lang="ts">
+import SearchIcon from '@/svgs/search-icon.vue';
 
 </script>
 
 <style scoped>
+input::before{
+
+}
 </style>
