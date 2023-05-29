7f747bf3fef728dbc7c5d268c79d474bc20395f2
“lcc” <chengcheng.li@51hgp.com>
2023-05-29 21:20:39 +0800

    store

--- src/components/GlobalSearch.vue
+++ src/components/GlobalSearch.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="flex justify-center">
-    <button class="flex items-center px-3 w-1/2 max-w-xs h-10 text-stone-400 rounded-full min-w-max ring-1 ring-slate-200 hover:ring-slate-300" @click="clickSearch">
-      <SearchIcon></SearchIcon>
-      <p class="flex-1 text-left">请输入...</p>
-      <p class="hidden sm:block">Ctrl+K</p>
-    </button>
-  </div>
-
-  <dialog v-modal-close class="p-0 backdrop:backdrop-blur-xxs bg-transparent" ref="searchPop" id="dialog">
-    <div class="w-[80vw] max-w-lg h-[70vh] rounded-lg bg-white">
-      <div class="flex items-center leading-8 text-slate-400 p-4 border-b-2  border-slate-300">
+  <div>
+    <div class="flex justify-center">
+      <button class="flex items-center px-3 w-1/2 max-w-xs h-10 text-stone-400 rounded-full min-w-max ring-1 ring-slate-200 hover:ring-slate-300" @click="clickSearch">
         <SearchIcon></SearchIcon>
-        <input type="text" v-model="keyWord" class=" flex-1 outline-none" placeholder="请输入...">
-      </div>
-      <div v-if="!searchRes.length" class="p-4 leading-8 text-center">暂无搜索结果</div>
-      <ul v-else>
-        <li v-for="(item, index) in searchRes" @click="jump(item)" :key="index" class=" px-8 py-4 leading-8 border-b-2 border-slate-100 hover:bg-slate-100">
-          {{item.name}}
-        </li>
-      </ul>
+        <p class="flex-1 text-left">请输入...</p>
+        <p class="hidden sm:block">Ctrl+K</p>
+      </button>
     </div>
-  </dialog>
+    <!-- backdrop:backdrop-blur-xxs -->
+    <dialog v-modal-close class="p-0  bg-transparent" ref="searchPop" id="dialog">
+      <div class="w-[80vw] max-w-lg h-[70vh] rounded-lg bg-white">
+        <div class="flex items-center leading-8 text-slate-400 p-4 border-b-2  border-slate-300">
+          <SearchIcon></SearchIcon>
+          <input type="text" v-model="keyWord" class=" flex-1 outline-none" placeholder="请输入...">
+        </div>
+        <div v-if="!searchRes.length" class="p-4 leading-8 text-center">暂无搜索结果</div>
+        <ul v-else>
+          <li v-for="(item, index) in searchRes" @click="jump(item)" :key="index" class=" px-8 py-4 leading-8 border-b-2 border-slate-100 hover:bg-slate-100">
+            {{item.name}}
+          </li>
+        </ul>
+      </div>
+    </dialog>
+
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -53,4 +56,28 @@ const jump = (item: searchDataItem)=>{
 </script>
 
 <style scoped>
+dialog{
+  opacity: 0;
+  /* visibility: hidden; */
+  transition: all 2s ;
+}
+dialog::backdrop{
+  transition: all 2s ;
+  backdrop-filter: blur(0);
+}
+dialog[open]{
+  opacity: 1;
+  visibility: visible;
+}
+dialog[open]::backdrop{
+  backdrop-filter: blur(1px);
+}
+
+/* dialog:hover{
+  opacity: 1;
+  backdrop-filter: blur(0);
+}
+dialog:hover::backdrop{
+  backdrop-filter: blur(1px);
+} */
 </style>
