19c21a148ee5c7a796a9874a5e337f024741b06f
Naxisigut <834525217@qq.com>
2023-06-02 01:42:08 +0800

    feat: github

--- src/views/github-shortcuts/index.vue
+++ src/views/github-shortcuts/index.vue
@@ -1,11 +1,22 @@
 <template>
-<!-- 
-  <div>
-    {{ profile.login }}
-  </div> -->
-  <ul class="grid grid-cols-3 gap-4 justify-items-center w-full mx-auto">
-    <li v-for="(item, index) in repos" :key="index" class=" p-4 shadow-md w-full ">
-      <a :href="item.html_url" class=" block w-full h-full  ">
+  <div class=" my-5 text-center text-sm leading-6">
+    <div class=" w-[150px] h-[150px] rounded-full overflow-hidden mx-auto border-stone-500 border">
+      <img :src="profile.avatar_url" alt="">
+    </div>
+    <h3 class=" text-lg">
+      <a class=" px-4 hover:bg-neutral-100" title="访问Github主页" :href="profile.html_url" target="_blank">{{ profile.name }}</a>
+    </h3>
+    <p class="">
+      <span>{{ profile.bio }} </span>
+    </p>
+    <p class="">{{ profile.location }}</p>
+
+  </div>
+
+
+  <ul class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center w-full mx-auto">
+    <li v-for="(item, index) in repos" :key="index" class=" p-4 shadow-md w-full rounded-md bg-neutral-100 hover:scale-110 transition-transform">
+      <a :href="item.html_url" target="_blank" class=" block w-full h-full  ">
         <h5 class="truncate font-medium text-lg">{{ item.name }}</h5>
         <p class=" font-light text-base">{{ item.description }}</p>
       </a>
@@ -37,8 +48,10 @@ function fetchGithub(url: string){
 
 fetchGithub('https://api.github.com/users/Naxisigut').then((res) => {
   profile.value = res
+  console.log("profile =", profile.value)
   return fetchGithub(profile.value.repos_url)
 }).then((res) => {
+  // console.log("repos =", repos)
   repos.value = res
 }).catch((err) => {
   console.log('err', err.message);
@@ -52,17 +65,4 @@ fetchGithub('https://api.github.com/users/Naxisigut').then((res) => {
 </script>
 
 <style scoped>
-ul li{
-  transition: all 1s ease-in;
-}
-ul{
-  /* justify-self: center; */
-  /* justify-items: center; */
-  /* transition: all 2s ease; */
-
-}
-/* ul{
-  grid-template-columns: repeat(auto-fill, 300px);
-} */
-
 </style>
