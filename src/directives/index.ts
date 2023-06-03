2beb3e74f45766f1405ba1c2a2c7a36e3811597f
“lcc” <chengcheng.li@51hgp.com>
2023-06-03 15:25:20 +0800

    fix: 指令规范

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -20,61 +20,75 @@
 //   unmounted(el, binding, vnode, prevVnode) {}
 // }
 
+/** directive
+ * 1. 若需要在指令的元素上绑定监听，不要使用简写形式，避免在元素updated时多次执行绑定
+ * 2. 若需要给window绑定监听，注意在元素销毁unmounted时移除监听
+ */
+
+import {Directive} from 'vue';
+type directiveObj = {
+  name: string,
+  directive: Directive,
+  [propName: string]: any
+}
 
 /* dialog标签 点击modal后关闭dialog */
 const dialogModalClose = {
   name: 'modal-close',
   directive:{
     mounted: (el:HTMLDialogElement) => {
-      console.log(1212, this);
       el.setAttribute('modal', 'true')
       el.addEventListener('click', (e)=>{
         if(!e.target)return
         const modalClicked = e.target.getAttribute('modal')
         if(modalClicked)el.close()
       })
-    }
+    },
   }
 }
 
 /* dialog标签 监听Ctrl+K 控制dialog显隐 */
-const dialogKeyOpen = {
+const dialogKeyOpen: directiveObj = {
   name: 'k-open',
+  preventDefault: (e: KeyboardEvent) => {
+    if(e.code === 'KeyK' && e.ctrlKey){
+      e.preventDefault()
+    }
+  },
+  ctrlKOpen: null,
   directive:{
     mounted: (el:HTMLDialogElement) => {
-      console.log('key open');(el:HTMLDialogElement) => {
-        el.setAttribute('modal', 'true')
-        el.addEventListener('click', (e)=>{
-          if(!e.target)return
-          const modalClicked = e.target.getAttribute('modal')
-          if(modalClicked)el.close()
-        })
-      }
       // 禁止ctrl+k默认行为
-      window.addEventListener('keydown', (e)=>{
-        if(e.code === 'KeyK' && e.ctrlKey){
-          e.preventDefault()
-        }
-      })
-  
-      // ctrl + k 打开dialog
-      window.addEventListener('keyup', (e)=>{
+      window.addEventListener('keydown', dialogKeyOpen.preventDefault)
+
+      dialogKeyOpen.ctrlKOpen = (e:KeyboardEvent) => {
+        console.log('k open');
         if(e.ctrlKey && e.code === 'KeyK' && el.getAttribute('open') === null){
           el.showModal()
         }
-      })
-    }
+      }
+
+      // ctrl + k 打开dialog
+      window.addEventListener('keyup', dialogKeyOpen.ctrlKOpen)
+    },
+    unmounted: () => {
+      // 元素销毁时移除全局监听
+      window.removeEventListener('keydown', dialogKeyOpen.preventDefault)
+      window.removeEventListener('keyup', dialogKeyOpen.ctrlKOpen)
+    },
   }
 }
 
-/* dialog标签 监听Ctrl+K 控制dialog显隐 */
+/* dialog标签  */
 const keySelect = {
   name: 'key-select',
-  directive:(el:HTMLInputElement) => {
-    console.log(111122);
-    el.addEventListener('keyup', (e) => {
-      console.log(e.repeat);
-    })
+  directive:{
+    mounted: (el:HTMLInputElement) => {
+      el.addEventListener('keyup', (e) => {
+        console.log(111);
+        // console.log(e.repeat);
+      })
+    },
   }
 }
 
