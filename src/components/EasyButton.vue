7cac93e4185f57c2036b12ae4d7ba2c667e54bee
“lcc” <chengcheng.li@51hgp.com>
2023-06-07 20:56:56 +0800

    feat: 二维码解析

---
+++ src/components/EasyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <slot name="default"></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<script scoped>
+
+</script>
--- src/views/qr-dealer/index.vue
+++ src/views/qr-dealer/index.vue
@@ -2,10 +2,10 @@
   <div class="flex items-center justify-center min-h-[inherit]">
     <div class="">
       <div class=" text-center">
-        <!-- <label for="" class="w-[80px]">功能选择：</label> -->
         <vaadin-combo-box class=" m-8" theme="align-right " .items="funcOpts" .value="currFunc" @change="change"></vaadin-combo-box>
       </div>
       <div class="flex">
+        <!-- 左：表单 -->
         <div class="flex flex-col justify-center p-4 w-[300px]">
           <!-- 尺寸 -->
           <div class="flex">
@@ -37,12 +37,15 @@
             <input type="text" v-model="params.size" class="flex-1 pl-2 w-0 outline-none border-slate-500 border-2 rounded-md text-slate-600">
           </div>
         </div>
-        <div v-if="(currFunc == '生成二维码' && params.text) || currFunc == '解析二维码'  " class=" flex flex-col justify-center p-4 border-l-2 border-slate-500">
+
+        <!-- 右：图片 -->
+        <div v-if="(currFunc == '生成二维码' && params.text) || currFunc == '解析二维码'" class=" flex flex-col justify-center p-4 border-l-2 border-slate-500">
           <img v-if="currFunc == '生成二维码'" :src="generatedBaseSrc" alt="">
           <img v-if="currFunc == '解析二维码' && uploadImgSrc" :src="uploadImgSrc " alt="" class=" w-[200px] h-[200px]"> 
           <input ref="uploader" type="file" hidden @change="getUploadImg">
           <button class=" rounded-md bg-gray-100 p-2 mt-4" @click="upload">上传图片</button>
         </div>
+        
       </div>
     </div>
   </div>
@@ -52,6 +55,7 @@
 import {ref, reactive, computed } from "vue";
 import { ComboBoxChangeEvent } from "@vaadin/combo-box";
 import QR from 'qrcode-base64'
+import QRParser from 'qrcode-parser';
 const params = reactive({
   text: '',
   typeNumber: 4,
@@ -68,19 +72,33 @@ const generatedBaseSrc = computed(()=>{
 })
 
 const funcOpts = reactive([ '生成二维码', '解析二维码' ])
-const currFunc = ref('生成二维码') // 默认值
+const currFunc = ref<string>('生成二维码') // 默认值
 const change = (e: ComboBoxChangeEvent<string>)=> currFunc.value = e.target.value
 
 const uploader = ref<HTMLInputElement | null>(null)
 const uploadImgSrc = ref<string>('')
 const upload = ()=>uploader.value?.click()
-const getUploadImg = (e)=>{
+const getUploadImg = (e: {target: {files: Array<File>}})=>{
   // console.log(e.target.files);
   if(!e.target.files.length)return
   const file = e.target.files[0]
   const reader = new FileReader()
   reader.readAsDataURL(file)
-  reader.onload = () => uploadImgSrc.value = reader.result as string
+  reader.onload = () => {
+    uploadImgSrc.value = reader.result as string
+    parse()
+  }
+}
+const parsedContent = ref<string>('')
+const parse = () => {
+  if(!uploader.value || !uploader.value.files || !uploader.value.files.length)return
+  const file = uploader.value.files[0]
+  QRParser(file).then((res) => {
+    parsedContent.value = res
+    params.text = res
+  }).catch((err) => {
+    console.log(err);
+  })
 }
 
 
