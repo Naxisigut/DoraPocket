7f00cb7546e746d955a3015fdf34256b4dcadf23
“lcc” <chengcheng.li@51hgp.com>
2023-06-12 21:03:03 +0800

    store

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -2,7 +2,13 @@ export const file2Url = (file: File) => {
   return new Promise<string>((resolve, reject) => {
     const reader = new FileReader()
     reader.onload = () => {
-      resolve(reader.result)
+      resolve(reader.result as string)
+    }
+    reader.onerror = (err) => {
+      reject(err)
+    }
+    reader.onabort = (err) => {
+      reject(err)
     }
     reader.readAsDataURL(file)
   })
