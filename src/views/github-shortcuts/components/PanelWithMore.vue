ab700089756056624add39b1b275c3bd0ddedc18
“lcc” <chengcheng.li@51hgp.com>
2023-06-30 20:59:29 +0800

    store

--- src/views/github-shortcuts/components/PanelWithMore.vue
+++ src/views/github-shortcuts/components/PanelWithMore.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="panel-with-more relative overflow-hidden">
+  <div class="panel-with-more relative overflow-hidden mb-8">
     <slot></slot>  
-    <div v-if="isFold" @click="toggle" class=" absolute bottom-0 w-full text-center backdrop-blur-sm">展开</div>
-    <span v-else @click="toggle" class=" sticky top-10">收起</span>
+    <div v-if="isFold" class=" absolute bottom-0 w-full text-center backdrop-blur-sm text-lg leading-10 ">
+      <c-button type="text" @click="toggle">展开</c-button>
+    </div>
+    <!-- <span v-else @click="toggle" class=" sticky top-10">收起</span> -->
   </div>
 </template>
 
 <script setup lang="ts">
+  import CButton from '@/components/CButton.vue';
   import { ref, computed} from 'vue';
   const isFold = ref<Boolean>(true)
 
--- src/views/github-shortcuts/index.vue
+++ src/views/github-shortcuts/index.vue
@@ -1,38 +1,44 @@
 <template>
-  <div v-if="profile" class=" pb-10 my-5 text-center text-sm leading-6">
-    <div class=" w-[150px] h-[150px] rounded-full overflow-hidden mx-auto border-stone-500 border">
-      <a title="访问Github主页" :href="profile.html_url" target="_blank">
-        <img :src="profile.avatar_url" alt="">
-      </a>
+  <div class="  mx-auto max-w-[960px]">
+    <div v-if="profile" class=" pb-10 my-5 text-center text-sm leading-6">
+      <div class=" w-[150px] h-[150px] rounded-full overflow-hidden mx-auto border-stone-500 border">
+        <a title="访问Github主页" :href="profile.html_url" target="_blank">
+          <img :src="profile.avatar_url" alt="">
+        </a>
+      </div>
+      <h3 class=" text-xl">
+        <a class=" px-4 hover:bg-neutral-200" title="访问Github主页" :href="profile.html_url" target="_blank">{{ profile.name }}</a>
+      </h3>
+      <p class="">{{ profile.bio }}</p>
+      <p class="">{{ profile.location }}</p>
     </div>
-    <h3 class=" text-xl">
-      <a class=" px-4 hover:bg-neutral-200" title="访问Github主页" :href="profile.html_url" target="_blank">{{ profile.name }}</a>
-    </h3>
-    <p class="">{{ profile.bio }}</p>
-    <p class="">{{ profile.location }}</p>
+    
+    <h2 class=" px-6 text-2xl">Repository</h2>
+    <PanelWithMore>
+      <ul class=" p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center w-full mx-auto">
+        <li v-for="(item, index) in repos" :key="index" class=" p-4 shadow-md w-full rounded-md bg-neutral-100 hover:scale-110 transition-transform">
+          <a :href="item.html_url" target="_blank" class=" block w-full h-full  ">
+            <h5 class="truncate font-medium text-lg">{{ item.name }}</h5>
+            <p class=" font-light text-base">{{ item.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </PanelWithMore>
+
+    <h2 class=" px-6 text-2xl">Stars</h2>
+    <PanelWithMore>
+      <ul class=" p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center w-full mx-auto">
+        <li v-for="(item, index) in starred" :key="index" class=" p-4 shadow-md w-full rounded-md bg-neutral-100 hover:scale-110 transition-transform">
+          <a :href="item.html_url" target="_blank" class=" block w-full h-full  ">
+            <h5 class="truncate font-medium text-lg">{{ item.name }}</h5>
+            <p class=" font-light text-base">{{ item.description }}</p>
+          </a>
+        </li>
+      </ul>
+    </PanelWithMore>
   </div>
 
-  <h2 class=" px-6 text-2xl">Repository</h2>
-  <ul class=" p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center w-full mx-auto">
-    <li v-for="(item, index) in repos" :key="index" class=" p-4 shadow-md w-full rounded-md bg-neutral-100 hover:scale-110 transition-transform">
-      <a :href="item.html_url" target="_blank" class=" block w-full h-full  ">
-        <h5 class="truncate font-medium text-lg">{{ item.name }}</h5>
-        <p class=" font-light text-base">{{ item.description }}</p>
-      </a>
-    </li>
-  </ul>
 
-  <h2 class=" px-6 text-2xl">Stars</h2>
-  <PanelWithMore>
-    <ul class=" p-6 grid sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center w-full mx-auto">
-      <li v-for="(item, index) in starred" :key="index" class=" p-4 shadow-md w-full rounded-md bg-neutral-100 hover:scale-110 transition-transform">
-        <a :href="item.html_url" target="_blank" class=" block w-full h-full  ">
-          <h5 class="truncate font-medium text-lg">{{ item.name }}</h5>
-          <p class=" font-light text-base">{{ item.description }}</p>
-        </a>
-      </li>
-    </ul>
-  </PanelWithMore>
 </template>
 
 <script setup lang="ts">
