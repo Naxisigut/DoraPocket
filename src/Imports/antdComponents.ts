53c07ab486c744fd7060923346b8ab331ebbb225
“lcc” <chengcheng.li@51hgp.com>
2023-07-07 21:42:11 +0800

    store

--- src/Imports/antdComponents.ts
+++ src/Imports/antdComponents.ts
@@ -2,10 +2,13 @@ import { App } from "vue";
 
 import "ant-design-vue/lib/select/style"; // 加载 CSS
 import Select from "ant-design-vue/lib/select"; // 加载 JS
+import "ant-design-vue/lib/input/style"; // 加载 CSS
+import Input from "ant-design-vue/lib/input"; // 加载 JS
 
 
 export default {
   install: (app: App<Element>) => {
     app.use(Select)
+    app.use(Input)
   }
 }
--- src/views/color-trans/index.vue
+++ src/views/color-trans/index.vue
@@ -4,13 +4,56 @@ export default {
 };
 </script>
 
+
 <template>
-  <div>
+  <div class="w-full p-[20px]">
+    <div class=" h-[80vh] shadow-lg border border-gray-200 rounded-xl">
+      <div class=" ">
+        <a-select
+          v-model:value="value"
+          style="width: 120px"
+          :options="opts"
+          @change="handleChange"
+        ></a-select>
+        <span>-></span>
+        <a-select
+          v-model:value="value"
+          style="width: 120px"
+          :options="opts"
+          @change="handleChange"
+        ></a-select>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { CColor } from '@/utils/color';
+
+type Option = {
+  label: string,
+  value: any,
+}
+
+const opts = reactive<Option[]>([
+  { label: 'test', value: 1},
+  { label: 'test2', value: 2},
+  { label: 'test3', value: 3},
+  { label: 'test4', value: 4},
+])
+
+const value = ref<string>('33fr')
+
+const handleChange = (val) => {
+  console.log(val);
+  console.log(2,value.value);
+}
 
 </script>
+
+
+
+
 
 <style></style>
