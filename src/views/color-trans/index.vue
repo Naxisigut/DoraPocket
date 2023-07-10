9426647cd465ce57fc7dbda43881e1becbaee105
Naxisigut <834525217@qq.com>
2023-07-11 01:13:08 +0800

    feat: 颜色转换器

--- src/views/color-trans/index.vue
+++ src/views/color-trans/index.vue
@@ -7,27 +7,17 @@ export default {
 
 <template>
   <div class=" h-[80vh] p-[20px] flex justify-center items-center">
-    <div class="  shadow-lg border border-gray-200 rounded-xl grid grid-cols-1 p-5 gap-5 ">
+    <div class=" w-[400px] shadow-lg border border-gray-200 rounded-xl grid grid-cols-1 p-5 gap-5 ">
       <div class=" flex items-center">
-        <a-input class=" flex-1 mr-5" addon-before="初始值" placeHolder="请输入十六进制或RGBA" v-model:value="initVal"></a-input>
-      </div>
-      <div></div>
-      <div class=" flex items-center">
-        <affix-span prefix="十六进制" class=" flex-1 mr-5">
-          <h4 class=" font-bold text-lg">{{ value.hex }}</h4>
-        </affix-span>
+        <a-input class=" flex-1 mr-5" addon-before="十六进制" placeHolder="请输入十六进制或RGBA" v-model:value="value.hex" @keyup.enter="confirm(value.hex)"></a-input>
         <c-button type="primary" size="mini" @click="copyText(value.hex)">复制</c-button>
       </div>
       <div class=" flex items-center">
-        <affix-span prefix="RGBA" class=" flex-1 mr-5">
-          <h4 class=" font-bold text-lg">{{ value.rgba }}</h4>
-        </affix-span>
+        <a-input class=" flex-1 mr-5" addon-before="RGBA" placeHolder="请输入十六进制或RGBA" v-model:value="value.rgba" @keyup.enter="confirm(value.rgba)"></a-input>
         <c-button type="primary" size="mini" @click="copyText(value.rgba)">复制</c-button>
       </div>
       <div class=" flex items-center">
-        <affix-span prefix="HSLA" class=" flex-1 mr-5">
-          <h4 class=" font-bold text-lg">{{ value.hsla }}</h4>
-        </affix-span>
+        <affix-span prefix="HSLA" :content="value.hsla" class=" flex-1 mr-5"></affix-span>
         <c-button type="primary" size="mini" @click="copyText(value.hsla)">复制</c-button>
       </div>
     </div>
@@ -35,46 +25,26 @@ export default {
 </template>
 
 <script setup lang="ts">
-import { reactive, ref, computed } from 'vue';
+import { reactive } from 'vue';
 import { CColor } from '@/utils/color';
 import { copyText } from '@/utils/other';
 
-
-// const value = reactive({
-//   hex: '',
-//   rgba: '',
-//   hsla: ''
-// })
-
-const value = computed(() => {
-  const newColor = new CColor(initVal.value)
-  // return {
-  //   hex: ''
-  // }
-  // if(!newColor.isValid){
-  //   Object.assign(value, {hex: '', rgba: '', hsla: ''})
-  // }
-  return {
-    hex: newColor.hex,
-    rgba: newColor.rgba,
-    hsla: newColor.hsla,
-  }
- 
+const value = reactive({
+  hex: '',
+  rgba: '',
+  hsla: '',
 })
 
-const initVal = ref<string>('')
-
-// const handleChange = (val: string)=>{
-//   const newColor = new CColor(val)
-//   if(!newColor.isValid){
-//     Object.assign(value, {hex: '', rgba: '', hsla: ''})
-//     return
-//   }
-//   value.hex = newColor.hex
-//   value.rgba = newColor.rgba
-//   value.hsla = newColor.hsla
-// }
-
+const confirm = (val: string)=>{
+  const newColor = new CColor(val)
+  if(!newColor.isValid){
+    return Object.assign(value, { hex: '', rgba: '', hsla: '' })
+  }
+  
+  value.hex = newColor.hex
+  value.rgba = newColor.rgba
+  value.hsla = newColor.hsla
+}
 </script>
 
 
