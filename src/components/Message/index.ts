33bc1db4b0cb42be1520103faea37e64c4b6b55a
“lcc” <chengcheng.li@51hgp.com>
2023-07-14 21:05:14 +0800

    feat: CMessage

---
+++ src/components/Message/index.ts
@@ -0,0 +1,5 @@
+import { createInstance } from "./instance";
+
+export const renderMsg = (config?) => {
+  createInstance(config)
+}
--- src/components/Message/instance.ts
+++ src/components/Message/instance.ts
@@ -1,35 +1,51 @@
 import { createApp } from 'vue';
 import Message from './index.vue';
 
-const createInstance = (cfg) => {
-  const config = cfg || {}
-  // 创建容器，设置外层class，消息计数
-  const messageNode = document.createElement('div')
-  messageNode.setAttribute('class', 'c-message')
+// 组件高度
+const height = 54
+const spaceHeight = 75
 
-  const height = 54
+// 重设位置
+const resetPosition = () => {
   const msgList = document.getElementsByClassName('c-message')
-  messageNode.style.top = `${msgList.length * height}px`
+  for(let index = 0; index < msgList.length ;index++){
+    msgList[index].style.top = `${index * height + spaceHeight}px`
+  }
+}
+
+// 获取当前位置
+const getCurrPosition = () => {
+  const msgList = document.getElementsByClassName('c-message')
+  return msgList.length * height + spaceHeight
+}
+
+// 创建msg应用实例
+export const createInstance = (cfg) => {
+  const config = cfg || {}
+  // 创建容器，设置外层class，用于计数
+  const msgNode = document.createElement('div')
+  msgNode.setAttribute('class', 'c-message')
+
+  // 设置高度，放置在已有的消息列表最下方
+  msgNode.style.top = `${getCurrPosition()}px`
+
+  msgNode.style.position = 'fixed'
+  msgNode.style.width = '100vw'
+  msgNode.style.transition = 'top .2s ease-out'
 
   // 创建实例并挂载到body
-  const msgApp = createApp(Message)
-  msgApp.mount(messageNode)
-  document.appendChild(messageNode)
+  const msgApp = createApp(Message, {
+    ...config,
+    destroy: ()=> destroyMsg()
+  })
+  msgApp.mount(msgNode)
+  document.body.appendChild(msgNode)
 
-  // 取消挂载并重设位置
-  const handleRemove = (params) => {
+  // 销毁组件方法
+  const destroyMsg = () => {
     msgApp.unmount()
-    document.removeChild(messageNode)
+    document.body.removeChild(msgNode)
     resetPosition()
   }
-
-  const resetPosition = () => {
-    const count = msgList.length
-    // for(const msg of msgList){
-    //   msg.style.top = 
-    // }
-  }
-
-  return msgApp
-  
+  return msgApp 
 }
