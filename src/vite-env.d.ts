ad700ac3b9d71d47cc8fb657375aa13a9b90ae7a
“lcc” <chengcheng.li@51hgp.com>
2023-07-28 20:54:26 +0800

    fix:  .vue文件找不到类型定义

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+
+// 解决router/index.ts 中 @/views/xxx/index.vue 报错找不到类型定义
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
