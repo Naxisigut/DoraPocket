8dd7da25f53594aaab5a142a4df25f72b7db5397
“lcc” <chengcheng.li@51hgp.com>
2023-08-04 14:40:21 +0800

    feat: easyFetch v1.1

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,3 +1,5 @@
+/* v1.1 */
+
 import { getConfig } from '@/views/gpt-chatter/useChatter';
 import { join2Query } from '@/utils/string';
 
@@ -10,52 +12,23 @@ export function fetchOpenAi(api: string, params: object){
       Authorization: `Bearer ${apiKey}`,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(params)
+    // body: JSON.stringify(params)
   }
-  return new Promise<any>((resolve, reject) => {
-    fetch(basePath + api, init).then((response) => {
-      // 无论状态码是否是200，均执行then。不同之处在于200时reponse.ok为true，否则为false
-      //.json()返回一个Promise，其结果为json parse后的对象
-      response.ok ? resolve(response.json()) : reject(new Error(response.status.toString()))
-    }).catch(()=>{
-      // 断网或超时才会进入catch
-      reject(new Error('network error'))
-    })
-  })
+
+  return easyFetch.post(basePath + api, params, init)
+  // return new Promise<any>((resolve, reject) => {
+  //   fetch(basePath + api, init).then((response) => {
+  //     // 无论状态码是否是200，均执行then。不同之处在于200时reponse.ok为true，否则为false
+  //     //.json()返回一个Promise，其结果为json parse后的对象
+  //     response.ok ? resolve(response.json()) : reject(new Error(response.status.toString()))
+  //   }).catch(()=>{
+  //     // 断网或超时才会进入catch
+  //     reject(new Error('network error'))
+  //   })
+  // })
 }
 
-/* 简单封装fetch */
-function fetchGithub(url: string){
-  return new Promise<any>((resolve, reject) => {
-    fetch(url).then((response) => {
-      // console.log(response);
-      // if(response.status !== 200){
-      //.json()返回一个Promise，其结果为json parse后的对象
-      response.ok ? resolve(response.json()) : reject(new Error(response.status.toString()))
-    })
-    .catch((err) => {
-      reject(err)
-    })
-    
-  })
-}
-
-/* fetch: 通用 */
-function fetch_general(url: string){
-  return new Promise<any>((resolve, reject) => {
-    fetch(url).then((response) => {
-      // console.log(response);
-      // if(response.status !== 200){
-      //.json()返回一个Promise，其结果为json parse后的对象
-      response.ok ? resolve(response.json()) : reject(new Error(response.status.toString()))
-    })
-    .catch((err) => {
-      reject(err)
-    })
-    
-  })
-}
-
+/* 简单fetch：get */
 const fetch_get = (url: string, params: Record<string, any> = {}) => {
   return fetch(url + join2Query(params), {
     method: 'GET',
@@ -64,6 +37,17 @@ const fetch_get = (url: string, params: Record<string, any> = {}) => {
   })
 }
 
+/* 简单fetch：post */
+const fetch_post = (url: string, params: Record<string, any> = {}, config:ResponseInit = {} ) => {
+  return fetch(url, {
+    ...config,
+    body: JSON.stringify(params)
+  }).then((response) => {
+    return response.json()
+  })
+}
+
 export const easyFetch = {
-  get: fetch_get
+  get: fetch_get,
+  post: fetch_post
 }
